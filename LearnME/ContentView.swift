b0435ab9b28d037c08d1840f098ba0a4b891178f
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-04-14 19:58:26 +0700

    Initial Commit

---
+++ LearnME/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnME
+//
+//  Created by Sufiandy Elmy on 14/04/20.
+//  Copyright © 2020 Sufiandy Elmy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
